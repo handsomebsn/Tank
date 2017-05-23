0c1d7d7b068a460b9ab41a70dcd5e1594676a2ca
phoenix <1903935168@qq.com>
2017-05-23 15:27:46 +0800

    图片变动

--- mapcell.cpp
+++ mapcell.cpp
@@ -10,8 +10,8 @@ void Mapcell::Display(QPainter &paint){
 int i,j;
 cal(style,i,j);
 if(!this->IsDisappear())
-paint.drawImage(m_rectSphere,blockimage,QRect(j*PICWIDTH,i*PICHEIGHT,PICWIDTH,PICHEIGHT));//优化代码，速度飞一般
-//paint.drawImage(m_rectSphere,QImage(":/images/map_block.png"),QRect(j*PICWIDTH,i*PICHEIGHT,PICWIDTH,PICHEIGHT));
+//paint.drawImage(m_rectSphere,blockimage,QRect(j*PICWIDTH,i*PICHEIGHT,PICWIDTH,PICHEIGHT));//优化代码，速度飞一般
+paint.drawImage(m_rectSphere,QImage(":/images/map_block.png"),QRect(j*PICWIDTH,i*PICHEIGHT,PICWIDTH,PICHEIGHT));
 
 
 }
